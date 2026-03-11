54655bb421bd5bc702c352857ea1fb8d8a15d77d
ronidomi <roni.domi001@umb.edu>
2026-03-11 13:45:26 -0400

    Bg changes, header and navbar complete

--- MAGE-Homepage/src/components/Header.tsx
+++ MAGE-Homepage/src/components/Header.tsx
@@ -3,8 +3,13 @@ import "../App.css"
 function Header() {
   return(
     <>
-      <div class="main-header">
-        <h1 class="noselect">MAGE</h1>
+      <div className="main-header">
+        <h1 className="noselect">MAGE</h1>
+        <p className="noselect">
+          Transform your sound into living, breathing visuals.
+          MAGE generates audio-reactive environments that <br/>
+          are never the same twice.
+        </p>
       </div>
     </>
   )
--- MAGE-Homepage/src/components/Navbar.tsx
+++ MAGE-Homepage/src/components/Navbar.tsx
@@ -1,13 +1,24 @@
+import Icon from "../assets/favicon.svg"
+
 function Navbar() {
   return(
     <>
-      <div class="nav">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#trending">Trending</a></li>
-          <li><a href="#engine">Engine</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+      <div className="nav">
+        <div className="mage-icon">
+          <img src={Icon} />
+        </div>
+        <div className="list">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#explore">Exlpore</a></li>
+            <li><a href="#engine">Create</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div className="login-btn">
+          <a href="login">Log In</a>
+        </div>
       </div>
     </>
   )
