b4db99eb00e25de4811ab67866431a0db3981f76
ronidomi <roni.domi001@umb.edu>
2026-03-11 12:04:21 -0400

    bg gradient and title text

---
+++ MAGE-Homepage/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "../App.css"
+
+function Header() {
+  return(
+    <>
+      <div class="main-header">
+        <h1 class="noselect">MAGE</h1>
+      </div>
+    </>
+  )
+}
+
+export default Header 
---
+++ MAGE-Homepage/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return(
+    <>
+      <div class="nav">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#trending">Trending</a></li>
+          <li><a href="#engine">Engine</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
